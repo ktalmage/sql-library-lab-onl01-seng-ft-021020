82197f2a5d2128d161be57230ea7ac33f5bd2173
Kyle Talmage <ktalmage85@gmail.com>
2020-03-11 14:16:43 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -8,7 +8,7 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Geralt", "No personality", "cylon", 2), (6, "Luke", "Do the right thing", "human", 2), (7, "Link", "save the princess", "cylon", 2), (8, "Quint", "get paid", "cylon", 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "No personality", "cylon", 2), (6, "Character Two", "Do the right thing", "human", 2), (7, "Character Three", "save the princess", "cylon", 2), (8, "Character Four", "get paid", "cylon", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
